d0fee72b7df4b8d3d281812745511ef511caa170
alaukikgupta <alaukikalok@gmail.com>
2025-03-03 13:50:10 -0500

    Initial portfolio website v21

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ document.addEventListener('DOMContentLoaded', function() {
                     projectsTabButton.click();
                 }
             });
+            setupProjectsTab();
         }
     });
     
